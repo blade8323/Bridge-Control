de0b54f9c928eca1b398b2891eca2e8968093141
Vladislav Sokolov <blade8323@mail.ru>
2020-08-07 13:04:13 +0300

    Initial Commit

---
+++ Bridge Control/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Bridge Control
+//
+//  Created by Admin on 07.08.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
